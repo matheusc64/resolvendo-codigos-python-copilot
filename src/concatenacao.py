24f3c61276c15960ab9702e0d8bab8ab29c2c92d
matheusc64 <matheusmello179@gmail.com>
2026-01-20 08:24:50 -0300

    Resolução de desafios em Python com apoio do GitHub Copilot

---
+++ src/concatenacao.py
@@ -0,0 +1,5 @@
+dado1 = input("Digite o primeiro dado: ")
+dado2 = input("Digite o segundo dado: ")
+
+resultado = dado1 + " " + dado2
+print("Resultado:", resultado)
---
+++ src/repeticao_texto.py
@@ -0,0 +1,5 @@
+texto = input("Digite um texto: ")
+quantidade = int(input("Digite quantas vezes repetir: "))
+
+print(texto * quantidade)
+
